hopez13/art

Speed up HInstruction::ReplaceUsesDominatedBy
We can pre-calculate the dominated blocks so that the dominance
calculation is faster overall. Methods which were affected by
the previous implementation see 10x speedup with this CL.

Bug: 319118425
Test: Locally compile the app in the bug
Test: art/test/testrunner/testrunner.py --host --64 --optimizing -b
Change-Id: I0e3b84485ea7082bec348b6852b5164e06aa7829

compiler/optimizing/nodes.cc
@@ -1575,14 +1575,44 @@ void HInstruction::ReplaceWith(HInstruction* other) {
 void HInstruction::ReplaceUsesDominatedBy(HInstruction* dominator,
                                           HInstruction* replacement,
                                           bool strictly_dominated) {
+  // Get the dominated blocks first to faster calculation of domination afterwards.
+  HGraph* graph = GetBlock()->GetGraph();
+  ArenaBitVector visited_blocks(graph->GetAllocator(),
+                                graph->GetBlocks().size(),
+                                /* expandable= */ false,
+                                kArenaAllocMisc);
+  visited_blocks.ClearAllBits();
+  ScopedArenaAllocator allocator(graph->GetArenaStack());
+  ScopedArenaQueue<const HBasicBlock*> worklist(allocator.Adapter(kArenaAllocMisc));
+  worklist.push(dominator->GetBlock());
+
+  while (!worklist.empty()) {
+    const HBasicBlock* current = worklist.front();
+    worklist.pop();
+    if (visited_blocks.IsBitSet(current->GetBlockId())) {
+      continue;
+    }
+    visited_blocks.SetBit(current->GetBlockId());
+    for (HBasicBlock* dominated : current->GetDominatedBlocks()) {
+      worklist.push(dominated);
+    }
+  }
+
   const HUseList<HInstruction*>& uses = GetUses();
   for (auto it = uses.begin(), end = uses.end(); it != end; /* ++it below */) {
     HInstruction* user = it->GetUser();
     size_t index = it->GetIndex();
     // Increment `it` now because `*it` may disappear thanks to user->ReplaceInput().
     ++it;
-    const bool dominated =
-        strictly_dominated ? dominator->StrictlyDominates(user) : dominator->Dominates(user);
+    bool dominated = false;
+    if (dominator->GetBlock() == user->GetBlock()) {
+      // Trickier case, call the other methods.
+      dominated =
+          strictly_dominated ? dominator->StrictlyDominates(user) : dominator->Dominates(user);
+    } else {
+      // Block domination.
+      dominated = visited_blocks.IsBitSet(user->GetBlock()->GetBlockId());
+    }
 
     if (dominated) {
       user->ReplaceInput(replacement, index);

yaoyoro-pj/android_art

compiler/optimizing/nodes.cc
@@ -1567,14 +1567,46 @@ void HInstruction::ReplaceWith(HInstruction* other) {
 void HInstruction::ReplaceUsesDominatedBy(HInstruction* dominator,
                                           HInstruction* replacement,
                                           bool strictly_dominated) {
+  // Get the dominated blocks first to faster calculation of domination afterwards.
+  HGraph* graph = GetBlock()->GetGraph();
+  ArenaBitVector visited_blocks(graph->GetAllocator(),
+                                graph->GetBlocks().size(),
+                                /* expandable= */ false,
+                                kArenaAllocMisc);
+  visited_blocks.ClearAllBits();
+  ScopedArenaAllocator allocator(graph->GetArenaStack());
+  ScopedArenaQueue<const HBasicBlock*> worklist(allocator.Adapter(kArenaAllocMisc));
+  HBasicBlock* dominator_block = dominator->GetBlock();
+  worklist.push(dominator_block);
+
+  while (!worklist.empty()) {
+    const HBasicBlock* current = worklist.front();
+    worklist.pop();
+    visited_blocks.SetBit(current->GetBlockId());
+    for (HBasicBlock* dominated : current->GetDominatedBlocks()) {
+      if (visited_blocks.IsBitSet(dominated->GetBlockId())) {
+        continue;
+      }
+      worklist.push(dominated);
+    }
+  }
+
   const HUseList<HInstruction*>& uses = GetUses();
   for (auto it = uses.begin(), end = uses.end(); it != end; /* ++it below */) {
     HInstruction* user = it->GetUser();
+    HBasicBlock* block = user->GetBlock();
     size_t index = it->GetIndex();
     // Increment `it` now because `*it` may disappear thanks to user->ReplaceInput().
     ++it;
-    const bool dominated =
-        strictly_dominated ? dominator->StrictlyDominates(user) : dominator->Dominates(user);
+    bool dominated = false;
+    if (dominator_block == block) {
+      // Trickier case, call the other methods.
+      dominated =
+          strictly_dominated ? dominator->StrictlyDominates(user) : dominator->Dominates(user);
+    } else {
+      // Block domination.
+      dominated = visited_blocks.IsBitSet(block->GetBlockId());
+    }
 
     if (dominated) {
       user->ReplaceInput(replacement, index);
@@ -1582,9 +1614,8 @@ void HInstruction::ReplaceUsesDominatedBy(HInstruction* dominator,
       // If the input flows from a block dominated by `dominator`, we can replace it.
       // We do not perform this for catch phis as we don't have control flow support
       // for their inputs.
-      const ArenaVector<HBasicBlock*>& predecessors = user->GetBlock()->GetPredecessors();
-      HBasicBlock* predecessor = predecessors[index];
-      if (dominator->GetBlock()->Dominates(predecessor)) {
+      HBasicBlock* predecessor = block->GetPredecessors()[index];
+      if (visited_blocks.IsBitSet(predecessor->GetBlockId())) {
         user->ReplaceInput(replacement, index);
       }
     }